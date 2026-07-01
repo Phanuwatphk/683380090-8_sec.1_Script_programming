28770c6ae8f12a0e165f41a270039e53f9a35be4
Phanuwatphk <flook5067@gmail.com>
2026-07-02 00:53:06 +0700

    Lab2_2 completed

--- Lab2_2.py
+++ Lab2_2.py
@@ -1,8 +1,17 @@
-age = int(input("Enter you age : "))
+while True:
+    age = int(input("Enter you age : "))
+    if age >= 0:
+        break
 
 if age < 5:
     print("You're too young for movies! Enjoy cartoons.")
 elif 5 <= age <= 12:
     print("G-rated or PG-rated movies.")
-elif 5 <= age <= 12:
+elif 13 <= age <= 17:
     print("PG-13 or R-rated (with parental guidance).")
+else:
+    print("Any movie rating.")
+
+option = input("Do you like action movie? (yes/no) : ")
+if option == "yes" and age >= 18:
+    print("You might enjoy the latest action blockbuster!")
