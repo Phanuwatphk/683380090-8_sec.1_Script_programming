419096ec35f93b83abb8a57a8244142bc97569e1
Phanuwatphk <flook5067@gmail.com>
2026-07-17 22:33:25 +0700

    delete test.py

--- Lab_3/test.py
+++
@@ -1,6 +0,0 @@
-# Buggy Snippet C: Misplaced Break
-for char in "hello":
-    print(char) # This will print 'h', 'e', 'l' then break
-    if char == 'l':
-        print("Found 'l', stopping!")
-        break
